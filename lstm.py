1edce5c1d4950fd0c7ff73fd2fe177828fc43cd1
yudistira <cbasemaster@gmail.com>
2020-05-08 04:29:00 +0700

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -20,8 +20,7 @@ import time
 import datetime
 from sklearn.metrics import mean_squared_error
 from math import sqrt
-#matplotlib inline
-#config InlineBackend.figure_format='retina'
+
 
 class MV_LSTM(torch.nn.Module):
     def __init__(self,n_features,seq_length):
@@ -73,10 +72,6 @@ from numpy import hstack
 def split_sequences(sequences, n_steps):
     X, y = list(), list()
     
-    #data2_scale=data2_scale[~(data2_scale[:,0:3]==0).all(1)]
-    #df_china=scaler.fit_transform(df[(is_china)][['confirmed','recovered','deaths']])
-    
-    #scaler = MinMaxScaler()
     for i in range(0,len(sequences),100):
         # find the end of this pattern
         end_ix = i + n_steps
@@ -88,16 +83,11 @@ def split_sequences(sequences, n_steps):
         # gather input and output parts of the pattern
         seq_x, seq_y = sequences[i:end_ix-33], sequences[end_ix-33:end_ix]
         
-        
-        #scaler.fit(seq_x)
-        
-        #print (i,len(sequences),seq_x.shape,seq_y.shape)
-        
         X.append(seq_x)
         y.append(seq_y)
     return array(X), array(y)
 
-
+#read training data#############################################################################################
 df = pd.read_csv('data/time-series-19-covid-combined-4.csv', skiprows=1)
 df.head()
 df.info()
@@ -105,6 +95,7 @@ df.columns = ['day','country', 'territory', 'lat','long','confirmed','recovered'
 
 is_china =  (df['country']=='China')
 
+#read testing data##############################################################################################
 df2 = pd.read_csv('data/time-series-19-covid-combined-4.csv', skiprows=1)
 df2.head()
 df2.info()
@@ -112,52 +103,30 @@ df2.columns = ['day','country', 'territory', 'lat','long','confirmed','recovered
 
 is_indonesia =  (df2['country']=='Indonesia')
 
-#data=df[(is_china)][['confirmed','lat','long','recovered','deaths']]
+#training data filtering#########################################################################################
 data=df[df.country.isin(['China','Germany','Australia','Brazil','US','Belgium','Spain','Italy','UK','France','Japan','Malaysia','Vietnam','Iran','UEA','Singapore','Thailand','Korea, South','Japan','Iran','Netherlands','Russia','Chile','India','Greece','Mexico','Mongolia','Philippines','New Zealand','South Africa','Botswana','Uruguay','Paraguay','Madagascar','Peru', 'Portugal', 'Denmark','Hungary','Kenya','Ireland','Israel','Norway','Mauritius','Rwanda','Iceland','Kazakhstan','Switzerland','Cyprus','Zimbabwe'])][['confirmed','lat','long','recovered','deaths']]
-#data=df[df.country.isin(['China'])][['confirmed','lat','long','recovered','deaths']]
-data2=df2[(is_indonesia)][['confirmed','lat','long','recovered','deaths']]
 
+
+#testing data filtering#########################################################################################
+data2=df2[(is_indonesia)][['confirmed','lat','long','recovered','deaths']]
 date=df2[(is_indonesia)][['day','confirmed']]
-print (date)
+
 date.day = pd.to_datetime(date.day,format='%Y%m%d', errors='ignore')
 date.set_index('day', inplace=True)
-#data['confirmed']=data['confirmed'].diff().fillna(0)
-#data2['confirmed']=data2['confirmed'].diff().fillna(0)
-
-
-#print (scaler.fit_transform(df[(is_china)][['confirmed','recovered','deaths']].values.astype(float).transpose(1,0)).shape)
-#data.values[:,0]=np.insert(np.diff(data.values[:,0]),0,0)
-
-
-
-
-
-
-# define input sequence
-#in_seq1 = array([x for x in range(0,100,10)])
-#in_seq2 = array([x for x in range(5,105,10)])
-#out_seq = array([in_seq1[i]+in_seq2[i] for i in range(len(in_seq1))])
-# convert to [rows, columns] structure
-#in_seq1 = in_seq1.reshape((len(in_seq1), 1))
-#in_seq2 = in_seq2.reshape((len(in_seq2), 1))
-#out_seq = out_seq.reshape((len(out_seq), 1))
-# horizontally stack columns
-#dataset = hstack((in_seq1, in_seq2, out_seq))
-#print (dataset.shape)
+################################################################################################################
 
 n_features = 5 # this is number of parallel inputs
 n_timesteps = 100 # this is number of timesteps
 
 
-print (data.values.shape)
-# convert dataset into input/output
+#input splitting################################################################################################
 X, Y = split_sequences(data.values, n_timesteps)
-#X_test, y_test = split_sequences(data2, n_timesteps)
+
 
 
 print (X.shape,Y.shape)
 
-#y_tile=np.tile(np.expand_dims(Y, axis=2), (1, 1,5))
+#normalization##################################################################################################
 alld=np.concatenate((X,Y),1)
 alld=alld.reshape(alld.shape[0]*alld.shape[1],alld.shape[2])
 
@@ -172,8 +141,7 @@ X=np.array(X)
 y=np.array(y)[:,:,0]
 
 
-#print (X,X_test)
-#quit()
+#training#########################################################################################
 
 mv_net = MV_LSTM(n_features,67).cuda()
 criterion = torch.nn.MSELoss() # reduction='sum' created huge loss value
@@ -187,29 +155,17 @@ mv_net.train()
 
 for t in range(train_episodes):
     
-    #np.random.shuffle(X)
-    
     for b in range(0,len(X),batch_size):
-        #idx = np.random.randint(32, size=32)
+       
         p = np.random.permutation(len(X))
-        
-        #print (X.shape)
-        #X=X[idx]
-        #y=y[idx]
-        
-        #y=sorted(y.all(), key=lambda k: idx)
         
         inpt = X[p][b:b+batch_size,:,:]
         target = y[p][b:b+batch_size,:]    
-        #print (inpt.shape)
+        
         x_batch = torch.tensor(inpt,dtype=torch.float32).cuda()    
         y_batch = torch.tensor(target,dtype=torch.float32).cuda()
-        #all_batch=torch.cat((x_batch, y_batch), 1).cuda()
-        #print (x_batch.shape, y_batch.shape,all_batch.shape)
-        
+       
         mv_net.init_hidden(x_batch.size(0))
-    #    lstm_out, _ = mv_net.l_lstm(x_batch,nnet.hidden)    
-    #    lstm_out.contiguous().view(x_batch.size(0),-1)
         
         output = mv_net(x_batch) 
         
@@ -223,18 +179,14 @@ for t in range(train_episodes):
         optimizer.step()        
         optimizer.zero_grad() 
     print('step : ' , t , 'loss : ' , loss.item())
-    
+
+
+#evaluation#########################################################################################################
 #data2x=data2[~(data2.confirmed==0)]
 data2x=data2
 truth = data2
 
-
-
-
-
 data2x.values[0:len(data2x),0]=np.insert(np.diff(data2x.values[0:len(data2x),0]),0,0)
-#scaler = MinMaxScaler()
-#scaler.fit(data2)
 data2x=scaler.transform(data2x) 
 
 
@@ -243,28 +195,12 @@ print (X_test.shape)
 mv_net.init_hidden(1)
 
 
-#print (pd.np.tile(data2.T,(100,1)).shape,X_pca[0:5].shape)
-#quit()
-
 lstm_out = mv_net(torch.tensor(X_test[:,-67:,:],dtype=torch.float32).cuda())
 lstm_out=lstm_out.reshape(1,100,1).cpu().data.numpy()
 
-#print (X_test[:,-61:,0],lstm_out)
-
-#lstm_out[0][0:58,0]=np.cumsum(lstm_out[0][0:58,0])
-#lstm_out = (np.repeat(np.expand_dims(lstm_out,axis=2), 5, axis=2))
-#actual_predictions = scaler.inverse_transform(np.array(lstm_out[0]))[:,0]
-#longe=np.concatenate([X_test[:,-25:],np.tile(lstm_out, (1, 1,5))],axis=1)
-#longe1=X_test[:,-25:]
-#longe2=np.tile(lstm_out, (1, 1,5))
-#longe[0][:,0]=np.cumsum(longe[0][:,0])
-#actual_predictions = scaler.inverse_transform(longe[0])[:,0]
-#actual_predictions1 = scaler.inverse_transform(longe1[0])[:,0]
-#actual_predictions2 = scaler.inverse_transform(longe2[0])[:,0]
 print (data2x[-67:,0],lstm_out)
 actual_predictions = scaler.inverse_transform(np.tile(lstm_out, (1, 1,5))[0])[:,0]
 
-#data2.values[0:len(data2),0]=np.insert(np.diff(data2.values[0:len(data2),0]),0,0)
 print (data2.values[-67:,0],actual_predictions)
 
 #actual_predictions=lstm_out
@@ -275,33 +211,16 @@ x2 = np.arange(0, 67, 1)
 x3 = np.arange(0, 100, 10)
 x4 = np.arange(0, 50, 1)
 
-#yinterp=np.insert(actual_predictions[:,0], 0, data2[-1,0] , 0)
-#print (yinterp.shape,x.shape) 
-#yinterp = UnivariateSpline(x, yinterp, s = 5e8)(x) 
-#print (data2[-2,0],yinterp[0]-(yinterp[0]-data2[-2,0]))
-#yinterp= (yinterp-(yinterp[0]-data2[-2,0]))
-#print (yinterp)
-#print (data2.values[-54:-1,0])
 
-##predcum=np.cumsum(np.append(np.diff(test[0:25,0]),np.diff(actual_predictions)))
-#predcum=np.cumsum(actual_predictions)
-#actualcum=np.cumsum(test[29:-11,0])
-#print (actualcum.shape)
-#test=np.insert(np.diff(test[:,0]),0,0)
-
+#save prediction
 with open('./lstmdata/predict_indo8.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
     pickle.dump(pd.Series(actual_predictions), f,protocol=2)
 
+#visualization####################################################################################################    
 fig, ax = plt.subplots() 
 plt.title('Days vs Confirmed Cases Accumulation')
 plt.ylabel('Confirmed')
-#plt.grid(True)
-#plt.autoscale(axis='x', tight=True)
-#plt.axvline(x=24, color='b', linestyle='--', label='The day pm2.5 decreases')
 
-#plt.axvline(x=50, color='g', linestyle='--', label='PSBB dimulai')
-#plt.axvline(x=53, color='yellow', linestyle='--', label='prediksi dimulai (14 April 2020)')
-#plt.plot(x4,test[33:,0])
 left, width = .25, .5
 bottom, height = .25, .5
 right = left + width
